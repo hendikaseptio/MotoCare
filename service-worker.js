af38e504bcb4f67f8e1815a3c0f386eeb4194c73
hendikaseptio <hendikaseptio11@gmail.com>
2025-12-24 14:47:38 +0700

    fix: bug

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,26 @@
-const CACHE_NAME = 'vehicle-maintenance-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('batre3lit-cache').then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './site.webmanifest',
+                './favicon/android-chrome-192x192.png',
+                './favicon/android-chrome-512x512.png',
+                './manifest.json',
+                'https://cdn.tailwindcss.com',
+                'https://unpkg.com/react@18/umd/react.production.min.js',
+                'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+                'https://unpkg.com/@babel/standalone/babel.min.js'
+            ]);
         })
-      );
-    })
-  );
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
 });
